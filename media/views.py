9bddef9953e64b6ecfe2bde4f3a835e934c8476f
Hossein Mahdiany <hossein.mahdiany@sotoon.ir>
2022-02-26 18:42:12 +0330

    delete extra comments in views.py

--- media/views.py
+++ media/views.py
@@ -9,15 +9,6 @@ from .models import Qcow
 class Home(TemplateView):
     template_name = 'home.html'
 
-# def upload(request):
-#     context = {}
-#     if request.method == 'POST':
-#         uploaded_file = request.FILES['document']
-#         fs = FileSystemStorage()
-#         name = fs.save(uploaded_file.name, uploaded_file)
-#         context['url'] = fs.url(name)
-#     return render(request, 'upload.html', context)
-
 
 def qcow_list(request):
     qcows = Qcow.objects.all()
